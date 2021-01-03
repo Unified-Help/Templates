2c22b8585d6ced2a0e4f3e9da7efaf3470bb188b
lennoly <lennonlimis@gmail.com>
2021-01-04 01:08:21 +0800

    Bootstrap for donate

--- donateItem.py
+++ donateItem.py
@@ -1,14 +1,17 @@
-from wtforms import Form, StringField, FloatField, IntegerField, RadioField, SelectField, TextAreaField, validators
+from wtforms import Form, StringField, FloatField, IntegerField, RadioField, SelectField, \
+    TextAreaField, validators
+from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.fields.html5 import DateField, TimeField, DecimalField
 
 
 class donateItem(Form):
-    donateToWho = SelectField('Donate to Who*', [validators.DataRequired()],
+    donateToWho = SelectField('Donate to Who*', validators=[DataRequired(message="Pick who to donate to!")],
                               choices=[('', 'Select'), ('Under Privileged', 'Under Privileged'),
                                        ('Physically Handicapped', 'Physically Handicapped'),
                                        ('Natural Disaster Survivors', 'Natural Disaster Survivors'),
-                                       ('Persons with Intellectual Disabilities', 'Persons with Intellectual Disabilities'),
+                                       ('Persons with Intellectual Disabilities',
+                                        'Persons with Intellectual Disabilities'),
                                        ('Seniors', 'Seniors'), ('Special Needs', 'Special Needs')], default='')
 
     # If donor inputs Item Donation
@@ -16,16 +19,16 @@ class donateItem(Form):
     itemType = SelectField('Item Type*', [validators.DataRequired()],
                            choices=[('Clothes', 'Clothes'), ('Furniture', 'Furniture'), ('Electronics', 'Electronics'),
                                     ('Books', 'Books')], default='C')
-    itemName = StringField("Name of Item (T-Shirt, Laptop etc.)*",
+    itemName = StringField("Name of Item*",
                            [validators.Length(min=1, max=150), validators.DataRequired()])
-    itemWeight = FloatField("Weight of Item (kg)*", [validators.NumberRange(min=0, max=150), validators.DataRequired()])
-    itemHeight = FloatField("Height of Item (m)*", [validators.NumberRange(min=0, max=150), validators.Optional()])
-    itemLength = FloatField("Length of Item (m)*", [validators.NumberRange(min=0, max=150), validators.Optional()])
-    itemWidth = FloatField("Width of Item (m)*", [validators.NumberRange(min=0, max=150), validators.Optional()])
+    itemWeight = FloatField("Weight of Item*", [validators.NumberRange(min=0, max=150), validators.DataRequired()])
+    itemHeight = FloatField("Height of Item*", [validators.NumberRange(min=0, max=150), validators.Optional()])
+    itemLength = FloatField("Length of Item*", [validators.NumberRange(min=0, max=150), validators.Optional()])
+    itemWidth = FloatField("Width of Item*", [validators.NumberRange(min=0, max=150), validators.Optional()])
     # itemImage = FileField('Picture of Item', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
 
     # Collection Types and Information
-    collectionDate = SelectField("Pick Collection Date", [validators.DataRequired()],
+    collectionDate = SelectField("Pick Collection Date*", [validators.DataRequired()],
                                  choices=[('01', '01'), ('02', '02'), ('03', '03'), ('04', '04'), ('05', '05'),
                                           ('06', '06'), ('07', '07'), ('08', '08'), ('09', '09'), ('10', '10'),
                                           ('11', '11'), ('12', '12'), ('13', '13'), ('14', '14'), ('15', '15'),
@@ -34,14 +37,14 @@ class donateItem(Form):
                                           ('26', '26'), ('27', '27'), ('28', '28'), ('29', '29'), ('30', '30'),
                                           ('31', '31')])
 
-    collectionMonth = SelectField("", [validators.DataRequired()],
+    collectionMonth = SelectField("Pick Collection Month*", [validators.DataRequired()],
                                   choices=[('01', 'January'), ('02', 'Feburary'), ('03', 'March'), ('04', 'April'),
                                            ('05', 'May'), ('06', 'June'), ('07', 'July'), ('08', 'August'),
                                            ('09', 'September'), ('10', 'October'), ('11', 'November'),
                                            ('12', 'December')])
 
-    collectionTime = StringField("Pick a Time (24hr format, e.g. 0000)*", [validators.Length(min=1, max=4),
-                                                                           validators.DataRequired()])
+    collectionTime = StringField("Pick a Time*", [validators.Length(min=1, max=4),
+                                                  validators.DataRequired()])
     collectionType = RadioField('Type of Collection*', choices=[('Drop Off', 'Drop Off'), ('We Pick Up', 'We Pick Up')],
                                 default='Drop Off')
 
